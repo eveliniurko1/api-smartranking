69fa431947fe763e33208ae5147945d168203ccd
eveliniurko1 <eveliniurko189@gmail.com>
2021-06-26 20:46:18 -0300

    remove default files app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PlayersModule } from './players/players.module';
-import { PlayersModule } from './eveliniurko1/players/players.module';
-import { PlayersModule } from './eveliniurko189@hotmail.com/players/players.module';
 
 @Module({
-  imports: [PlayersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
